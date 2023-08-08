adb9bfe58047023a7441ff4cec8f797ceab6c3e6
jerisan <jerilynn.sancho@gmail.com>
2023-08-07 20:52:16 -0500

    buttons-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+//BOTONES QUE SE ACTUALIZAN A LA VEZ
+export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Contadores que se actualizan juntos</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
 
-export default App;
+function MyButton({ count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Hiciste clic {count} veces
+    </button>
+  );
+}
+
+//CONTADORES QUE SE ACTUALIZAN POR SEPARADO
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Contadores que se actualizan separadamente</h1>
+//       <MyButton />
+//       <MyButton />
+//     </div>
+//   );
+// }
+
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Hiciste clic {count} veces
+//     </button>
+//   );
+// }
